0bb3ecc4ce8e0ea9fac969e30c5de9e745690824
renataeugenia <eugeniacomercial@gmail.com>
2022-04-07 15:41:04 -0300

    aplicação angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LoginPageComponent } from './paginas/login-page/login-page.component';
+import { RegisterPageComponent } from './paginas/register-page/register-page.component';
+import { FramePaginaComponent } from './paginas/frame.pagina';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: FramePaginaComponent,
+    children:[]
+  },
+
+  {path: 'principal', component:NavbarComponent },
+  {path: 'leitor/estante/livro', component: RegisterPageComponent},
+  {path: 'login/estante/livro', component:LoginPageComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LoginPageComponent } from './paginas/login-page/login-page.component';
+import { RegisterPageComponent } from './paginas/register-page/register-page.component';
+import { FramePaginaComponent } from './paginas/frame.pagina';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LoginPageComponent,
+    RegisterPageComponent,
+    FramePaginaComponent
   ],
   imports: [
     BrowserModule,
